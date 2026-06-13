4c4f2539b0c1fd0a8753c9b9d76d7d93d8d33c93
wael <ahmed.wael2048@gmail.com>
2026-06-13 14:18:27 +0300

    lab_three_a

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,51 @@
 class PostsController < ApplicationController
-  # your code goes here
+  # GET /posts
+  def index
+    @posts = Post.all
+  end
+
+  # GET /posts/:id
+  def show
+    @post = Post.find(params[:id])
+  end
+
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post, notice: "Post was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # GET /posts/:id/edit
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  # PATCH /posts/:id
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post, notice: "Post was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /posts/:id
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: "Post was successfully deleted."
+  end
+  
 end
---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+    validates :name, presence: true
+    validates :dob, presence: true
+    validates :email, presence: true
+    validates :phone_number, presence: true
+
+    validates :email,
+                uniqueness: true,
+                format: {
+                with: URI::MailTo::EMAIL_REGEXP
+                }
+end
